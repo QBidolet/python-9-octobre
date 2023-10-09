644ea09004de64a7dfe4fe2581f466e5502756a2
Quentin <quentin.bidolet@gmail.com>
2023-10-09 12:06:50 +0200

    - print

---
+++ cours/2-premier-programme/01_comment.py
@@ -0,0 +1,8 @@
+# Mon premier commentaire.
+# Une autre ligne.
+
+"""
+Commentaire multiligne.
+Une autre ligne.
+Docstring
+"""
---
+++ cours/2-premier-programme/02_print.py
@@ -0,0 +1,17 @@
+# Fonction print : pour afficher du texte à l'utilisateur dans le terminal.
+print("Hello World!")
+
+# Séparateur
+print('Hello', "World!", sep="$")
+
+# End
+print("Hello", "World!", sep="$", end="**\n\n")
+
+# Ecrire deux lignes avec un seul print avec le \n.
+print("Première ligne.\nDeuxième ligne.")
+
+# Ou alors avec un string multiligne.
+print("""
+Première ligne.
+Deuxième ligne.
+""")
