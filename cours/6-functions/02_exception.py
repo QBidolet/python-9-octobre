b1b925712544409da7c463a4eacb46c966ff664a
Quentin <quentin.bidolet@gmail.com>
2023-10-10 16:31:23 +0200

    - exceptions

---
+++ cours/6-functions/02_exception.py
@@ -0,0 +1,30 @@
+# # Lever une exception
+# nombre = int(input("Entrez un nombre\n"))
+# print(nombre + 4)
+
+# Gérer l'exception
+# try:
+#     nombre = int(input("Entrez un nombre\n"))
+# except ValueError:
+#     print("Veuillez rentrer un nombre valide.")
+
+# Double exception
+# try:
+#     numerateur = int(input("Entrez un numérateur.\n"))
+#     denominateur = int(input("Entrez un dénominateur.\n"))
+#     resultat = numerateur / denominateur
+# except ValueError:
+#     print("Veuillez entrer un nombre valide.")
+# except ZeroDivisionError:
+#     print("Veuillez entrer un dénominateur différent de 0.")
+# else:
+#     print(resultat)
+#
+
+# Lever exception
+try:
+    x = int(input("Saisissez un nombre positif."))
+    if x < 0:
+        raise ValueError("La valeur ne doit pas être négative.")
+except ValueError as e:
+    print(e)
--- exercices/exercice_4.py
+++ exercices/exercice_4.py
@@ -3,3 +3,8 @@ Ecrire un programme qui affiche les 20 premiers termes de la table de multiplica
 Inclure ce programme dans une fonction et créer des paramètres pour rendre le nombre de terme et la table choisie variable.
 
 """
+def generer_table_multiplication(table=7, terme=20):
+    for nombre in range(1, terme + 1):
+        print(f"{table} X {nombre} = {table*nombre}")
+
+generer_table_multiplication()
