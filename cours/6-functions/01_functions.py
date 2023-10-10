bbcfdb51ffc380118116d934f6cc83dd01d6a68a
Quentin <quentin.bidolet@gmail.com>
2023-10-10 15:41:25 +0200

    - functions

--- cours/6-functions/01_functions.py
+++ cours/6-functions/01_functions.py
@@ -68,3 +68,71 @@ def somme(*args):
 
 resultat = somme(5, 58, 59, 54, 213, 458, 95, 1, -999)
 print(resultat)
+
+
+# unpacking
+def unpacking():
+    return 1, 2
+
+
+a, b = unpacking()
+
+# fonctions anonymes
+print("#" * 25)
+
+
+def double(a):
+    return a * 2
+
+
+print(double(2))
+
+liste = [1, 2, 3, 4]
+nouvelle_liste = []
+for nombre in liste:
+    nouvelle_liste.append(double(nombre))
+print(nouvelle_liste)
+
+# map
+mon_map = map(double, liste)
+print(list(mon_map))
+
+mon_map = map(lambda a: a * 2, liste)
+print(list(mon_map))
+
+# filter
+mon_filter = filter(lambda x: x % 2 == 0, liste)
+print(list(mon_filter))
+
+
+# closure
+def fonction_externe():
+    nb = 5
+
+    def fonction_interne(x):
+        return x + nb
+
+    return fonction_interne
+
+
+fonction_interne = fonction_externe()
+print(fonction_interne(10))
+
+print("#" * 25)
+
+# fonctions génératrices
+for i in range(6):
+    print(i)
+
+
+def range_2(n):
+    compteur = 0
+    while compteur < n:
+        yield compteur
+        compteur += 1
+
+
+print("#" * 25)
+
+for i in range_2(6):
+    print(i)
---
+++ exercices/exercice_4.py
@@ -0,0 +1,5 @@
+"""
+Ecrire un programme qui affiche les 20 premiers termes de la table de multiplication de 7.
+Inclure ce programme dans une fonction et créer des paramètres pour rendre le nombre de terme et la table choisie variable.
+
+"""
