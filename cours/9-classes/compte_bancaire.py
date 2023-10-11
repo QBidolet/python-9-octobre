e04b47a12813f5785db26525a762245ca544e57d
Quentin <quentin.bidolet@gmail.com>
2023-10-11 15:51:00 +0200

    - compte bancaire

--- cours/9-classes/compte_bancaire.py
+++ cours/9-classes/compte_bancaire.py
@@ -12,3 +12,35 @@
     BONUS :
     De mettre "DUPONT" et 1000 en valeur par défaut respectivement et le solde.
 """
+
+
+class CompteBancaire:
+    def __init__(self, nom="DUPONT", solde=1000):
+        self.nom = nom
+        self.solde = solde
+
+    def deposer(self, somme):
+        if somme > 0:
+            self.solde += somme
+        else:
+            print("Veuillez rentrer une valeur positive.")
+
+    def retirer(self, somme):
+        if somme > 0 and somme < self.solde:
+            self.solde -= somme
+        else:
+            print("Veuillez rentrer une valeur positive.")
+
+    def __str__(self):
+        return f"Titulaire du compte : {self.nom}\n" \
+               f"Solde : {self.solde}"
+
+
+compte_bancaire = CompteBancaire("BIDOLET")
+print(compte_bancaire)
+
+compte_bancaire.deposer(150)
+print(f"Nouveau solde : {compte_bancaire.solde}")
+compte_bancaire.retirer(100)
+print(f"Nouveau solde : {compte_bancaire.solde}")
+print(compte_bancaire)
