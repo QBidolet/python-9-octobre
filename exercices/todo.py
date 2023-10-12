2a3522766cc6f1dc3698374d572945495a80d2d3
Quentin <quentin.bidolet@gmail.com>
2023-10-12 15:27:46 +0200

    - todo correction

--- exercices/todo.py
+++ exercices/todo.py
@@ -1,64 +1,96 @@
-"""
-    Objectif : Créer une TODO liste sur terminal.
-    Découper votre programme en fonction.
-    1. une fonction main() : pour naviguer dans un menu
-    2. une fonction lister_tache() pour afficher toutes les tâches
-    3. une fonction supprimer_tache(titre)
-    4. une fonction marquer_comme_fait(titre)
-
-    Créer une liste pour stocker vos tâches.
-    Chaque tâche est représenté par un dictionnaire avec deux clés
-    : le titre et le statut ("à faire" ou "terminée")
-"""
-
-taches = [
-    {
-        "titre": "Faire le TP",
-        "statut": "à faire",
-    },
-    {
-        "titre": "Faire les courses",
-        "statut": "terminée"
-    }
-]
+taches = []
 
 
-def lister_tache():
-    """
-    Afficher toutes les tâches de ma structure de données "taches" sur le terminal.
-    """
-    print("\n*** Contenu de la TODO liste ***\n")
-    for indice, tache in enumerate(taches):
-        print(f"N°{indice + 1} - {tache['titre']} - {tache['statut']}")
+def lister_taches():
+    if not taches:
+        print("Aucune tâche.")
+    else:
+        for indice, tache in enumerate(taches):
+            print(f"N°{indice + 1} - "
+                  f"{tache['titre']} "
+                  f"- statut : {tache['statut']}")
+
+
+def ajouter_tache(titre):
+    taches.append(
+        {
+            "titre": titre,
+            "statut": "à faire"
+        }
+    )
 
 
 def supprimer_tache(titre):
     for tache in taches:
         if tache["titre"] == titre:
             taches.remove(tache)
-            print("La tâche a bien été supprimé.")
             return
     print("La tâche n'a pas été trouvé.")
 
 
+def marquer_comme_fait(titre):
+    for tache in taches:
+        if tache["titre"] == titre:
+            tache["statut"] = "terminée"
+            return
+    print("La tâche n'a pas été trouvé.")
+
+
+def sauvegarder_taches(fichier="taches.txt"):
+    with open(fichier, "w") as file:
+        for tache in taches:
+            file.write(f"{tache['titre']}|{tache['statut']}\n")
+    print("Tâches sauvegardées.")
+
+
+def charger_taches(fichier="taches.txt"):
+    try:
+        with open(fichier, 'r') as file:
+            taches.clear()
+            lignes = file.readlines()
+            for ligne in lignes:
+                titre, statut = ligne.strip().split('|')
+                taches.append(
+                    {
+                        "titre": titre,
+                        "statut": statut
+                    }
+                )
+    except FileNotFoundError:
+        print("Le fichier n'a pas été trouvé.")
+
 def main():
     while True:
         print("""
-            Bienvenue dans la TODO liste.
-            1. Lister les tâches
-            2. Supprimer une tâche
-            3. Marquer comme fait            
+        Bienvenue dans ma TODO liste.
+        Que souhaitez-vous faire ?
+        1. Lister toutes les tâches
+        2. Ajouter une tâche
+        3. Supprimer une tâche
+        4. Marquer une tâche comme terminée
+        5. Sauvegarder dans un fichier
+        6. Charger depuis un fichier
+        7. Quitter
         """)
+        choix = input()
 
-        reponse = input()
-        if reponse == "1":
-            lister_tache()
-        elif reponse == "2":
-            titre = input("Veuillez saisir le titre d'une tâche à supprimer.\n")
+        if choix == "1":
+            lister_taches()
+        elif choix == "2":
+            titre = input("Entrez le nom de la tâche.\n")
+            ajouter_tache(titre)
+        elif choix == "3":
+            titre = input("Entrez le nom de la tâche.\n")
             supprimer_tache(titre)
-        elif reponse == "3":
-            pass
+        elif choix == "4":
+            titre = input("Entrez le nom de la tâche.\n")
+            marquer_comme_fait(titre)
+        elif choix == "5":
+            sauvegarder_taches(fichier="taches.txt")
+        elif choix == "6":
+            charger_taches(fichier="taches.txt")
+        else:
+            break
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
